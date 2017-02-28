0fa67f256a272492c4f2e24cffe27d8324927388
Sam Fessler <fesslersam@gmail.com>
2017-02-28 14:44:39 -0600

    added form barebones

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 var samApp = angular.module('samApp', ['ui.bootstrap']);
 
 var gem=[
@@ -6,7 +8,8 @@ var gem=[
     price: 400,
     description: 'this is a gem, yeee',
     inStock: false,
-    images:[{fullSize:'/images/gem-01.gif'},{fullSize:'/images/gem-02.gif'},]
+    images:[{fullSize:'/images/gem-01.gif'},{fullSize:'/images/gem-02.gif'},],
+    reviews:[{stars:5, body: 'yeeee',author: 'joey@bannanas.org'}]
     
     },
     
@@ -15,15 +18,17 @@ var gem=[
     price: 1,
     description: 'this is not a gem, tis an apple',
     inStock: true,
-    images:[{fullSize:'/images/gem-05.gif'},{fullSize:'/images/gem-06.gif'},]
+    images:[{fullSize:'/images/gem-05.gif'},{fullSize:'/images/gem-06.gif'},],
+    reviews:[{stars:7, body: 'wtf bruh',author: 'manananay@bannanas.org'}]
     
     },
     {
     name: 'gem',
     price: 200,
     description: 'this gem is an amoeba',
-    inStock: false,
-    images:[{fullSize:'/images/gem-07.gif'},{fullSize:'/images/gem-08.gif'},]
+    inStock: true,
+    images:[{fullSize:'/images/gem-07.gif'},{fullSize:'/images/gem-08.gif'},],
+    reviews:[{stars:9, body: 'it is an amoeba',author: 'buggythebug@bannanas.org'}]
     
     },
     {
@@ -31,7 +36,8 @@ var gem=[
     price: 10000,
     description: 'this is a rare condition that turns your skin blue',
     inStock: true,
-    images:[{fullSize:'/images/gem-09.gif'},{fullSize:'/images/gem-10.gif'},]
+    images:[{fullSize:'/images/gem-09.gif'},{fullSize:'/images/gem-10.gif'},],
+    reviews:[{stars:0, body: 'turned my skin blue',author: 'lepper@bannanas.org'}]
     
     }
 ];
@@ -39,9 +45,31 @@ var gem=[
 samApp.controller('samController',function($scope){
     
     $scope.model = gem;
+    
+    $scope.cart = [];
+    
+    $scope.addProduct = function(product){
+        $scope.cart.push(product);
+    }
 
     $scope.initialize = function(){
         console.log("initialized now");
     };
+});
+
+angular.module('samApp').controller("panelController", function($scope){
+    
+    $scope.tab =1;
+    
+    $scope.selectTab = function(tab){
+        
+        $scope.tab=tab;
+    }
+    
+//    $scope.isSelected = function(tab){
+//        return $scope.tab === tab;
+//    }
+    
+    
 });
 
